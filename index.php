86a092ae61b671b890b39bca8c225b013c01c954
Hatem Ayman <hatem.ayman.508@gmail.com>
2026-03-14 15:42:29 +0200

    fix: sql injection vulnerability

--- index.php
+++ index.php
@@ -11,6 +11,9 @@ $config = require 'config.php';
 
 $db = new Database($config['database']);
 
-$posts = $db->query('select * from posts')->fetchAll();
+$id = $_GET['id']; 
+$posts = $db->query('select * from posts where id = ?', [$id])->fetchAll(); 
+
+// NEVER put user input directly into CRUD SQL. Always use prepared statements.
 
 // dd($posts);
