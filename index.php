403c2067909f5dd640419fa40d812c3823329cae
Hatem Ayman <hatem.ayman.508@gmail.com>
2026-03-14 14:53:07 +0200

    refactor: add a database class for connecting with mysql databases

--- index.php
+++ index.php
@@ -2,4 +2,13 @@
 
 require 'functions.php';
 
+require 'Database.php';
+
 require 'router.php';
+
+// connect to mysql database
+$db = new Database();
+
+$posts = $db->query('select * from posts')->fetchAll(PDO::FETCH_ASSOC);
+
+dd($posts);
