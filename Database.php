ff6afce1556fb7f6e9a8f8e80c2002119f98a498
Hatem Ayman <hatem.ayman.508@gmail.com>
2026-03-14 15:15:04 +0200

    refactor: use config file for flexible environment

--- Database.php
+++ Database.php
@@ -1,17 +1,21 @@
 <?php
 
-class Database {
+class Database
+{
 
     public $connection;
 
-    public function __construct()
+    public function __construct($config, $username = 'root', $password = '')
     {
-        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+        $dsn = 'mysql:' . http_build_query($config, '', ';'); // to build connection string with key-value pairs and delimeter ;
 
-        $this->connection = new PDO($dsn);
+        $this->connection = new PDO($dsn, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC // default fetch mode
+        ]);
     }
-    
-    public function query($query) {
+
+    public function query($query)
+    {
         $statement = $this->connection->prepare($query);
 
         $statement->execute();
--- index.php
+++ index.php
@@ -7,8 +7,10 @@ require 'Database.php';
 require 'router.php';
 
 // connect to mysql database
-$db = new Database();
+$config = require 'config.php';
 
-$posts = $db->query('select * from posts')->fetchAll(PDO::FETCH_ASSOC);
+$db = new Database($config['database']);
 
-dd($posts);
+$posts = $db->query('select * from posts')->fetchAll();
+
+// dd($posts);
