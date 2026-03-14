e6881ba701b03e0e9583778c8737f00b0772381d
Hatem Ayman <hatem.ayman.508@gmail.com>
2026-03-14 13:13:21 +0200

    fix: dynamic styling of navigation bar on select

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -14,12 +14,12 @@
                         </a>
 
                         <a href='/about'
-                            class="rounded-md px-3 py-2 text-sm font-medium <?= urlIs('/about.php') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' ?>">
+                            class="rounded-md px-3 py-2 text-sm font-medium <?= urlIs('/about') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' ?>">
                             About
                         </a>
 
                         <a href='/contact'
-                            class="rounded-md px-3 py-2 text-sm font-medium <?= urlIs('/contact.php') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' ?>">
+                            class="rounded-md px-3 py-2 text-sm font-medium <?= urlIs('/contact') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' ?>">
                             Contact
                         </a>
                     </div>
