a2f93e580703a6b2e08c1887225fe84ae15cff05
Hatem Ayman <hatem.ayman.508@gmail.com>
2026-03-19 20:25:05 +0200

    feat: add form validations

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,15 +1,26 @@
 <?php
 
+require 'Validator.php';
+
 $config = require('config.php');
 $db = new Database($config['database']);
 
 $heading = 'New Note';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $db->query('INSERT INTO notes(body,user_id) VALUES(:body, :user_id)', [
-        'body' => $_POST['body'],
-        'user_id' => $USER_ID
-    ]);
+    $errors = [];
+
+    if (!Validator::string($_POST['body'], 1, 10)) {
+        $errors['body'] = 'A body of no more than 1000 charachters is required';
+    }
+
+    if (empty($errors)) {
+
+        $db->query('INSERT INTO notes(body,user_id) VALUES(:body, :user_id)', [
+            'body' => $_POST['body'],
+            'user_id' => $USER_ID
+        ]);
+    }
 }
 
 
